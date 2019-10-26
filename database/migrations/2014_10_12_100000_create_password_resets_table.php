e4d9b18861dc6ec3b3c6552d7f5cc5a36509e2a6
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-10-26 11:48:59 -0300

    Ajustes nas migrações
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,13 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+        });
+        Schema::table('password_resets', function (Blueprint $table) {
+            $table->foreign('email')->references('email')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
