b98cca32eaa706c6e0d5ba53f627df2ea5137f31
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-10-26 21:59:42 -0300

    CTA da página principal
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -11,9 +11,13 @@
                 <div class="col-6">
                     <h2>Tenha todas as suas fotos na palma da sua mão. Literalmente.</h2>
                     <p>Revela é a plataforma que te ajuda a liberar espaço no seu celular, colocando-as em um álbum digital todo mês.</p>
-                    <a href="#!" class="btn btn-primary btn-main landing-cta">
-                        Começar agora
+                    <a class="cta-home mt-3" href="#!">
+                        <span class="content">Começar agora</span>
+                        <span class="icon"><i class="fa fa-arrow-right" aria-hidden="true"></i></span>
                     </a>
+                    {{-- <a href="#!" class="btn btn-primary btn-main landing-cta">
+                        Começar agora
+                    </a> --}}
                 </div>
             </div>
         </div>
