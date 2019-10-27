e25eafa652e2a989a9f09cf9589fe5819336765d
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-10-26 23:37:39 -0300

    Seção do passo a passo
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -39,6 +39,66 @@
         </section>
 
     </article>
+    
+    <section id="steps-section">
+
+        <div class="container">
+
+            <h2>Veja como é fácil começar</h2>
+
+            <div id="timeline">
+                <div class="timeline-item">
+                    <div class="timeline-icon">
+                        1
+                    </div>
+                    <div class="timeline-content right">
+                        <h2>Crie uma conta gratuíta</h2>
+                        <p>
+                            Você pode criar uma conta com Facebook e Google caso prefira
+                        </p>
+                    </div>
+                </div>
+
+                <div class="timeline-item">
+                    <div class="timeline-icon">
+                        2
+                    </div>
+                    <div class="timeline-content left">
+                        <h2>Escolha o seu plano</h2>
+                        <p>
+                            Temos uma variedade de planos para melhor atender suas necessidades
+                        </p>
+                    </div>
+                </div>
+            
+                <div class="timeline-item">
+                    <div class="timeline-icon">
+                        3
+                    </div>
+                    <div class="timeline-content right">
+                        <h2>Escolha suas fotos favoritas</h2>
+                        <p>
+                            Essa é a parte que você seleciona suas melhores fotos do seu celular
+                        </p>
+                    </div>
+                </div>
+            
+                <div class="timeline-item">
+                    <div class="timeline-icon">
+                        4
+                    </div>
+                    <div class="timeline-content left">
+                        <h2>Receba seu álbum em casa</h2>
+                        <p>
+                            Agora é só esperar pelo seu álbum personalizado no conforto da sua casa
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
 
 
 @endsection
