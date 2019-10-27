223f2101707774ec33abbc7491392c4d68bc8f55
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-10-26 23:00:02 -0300

    Resto da seção principal

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -4,23 +4,41 @@
 
 @section('content')
 
-    <section id="hero-section">
-    
+    <article id="main-section">
+
         @include('includes.navbar')
 
-        <div class="container">
-            <div class="row">
-                <div class="col-6">
-                    <h2>Tenha todas as suas fotos na palma da sua mão. Literalmente.</h2>
-                    <p>Revela é a plataforma que te ajuda a liberar espaço no seu celular, colocando-as em um álbum digital todo mês.</p>
-                    <a class="cta-home mt-3" href="#!">
-                        <span class="content">Começar agora</span>
-                        <span class="icon"><i class="fa fa-arrow-right" aria-hidden="true"></i></span>
-                    </a>
+        <section id="hero-section">
+    
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <h2>Tenha todas as suas fotos na palma da sua mão. Literalmente.</h2>
+                        <p>Revela é a plataforma que te ajuda a liberar espaço no seu celular, colocando-as em um álbum digital todo mês.</p>
+                        <a class="cta-home mt-3" href="#!">
+                            <span class="content">Começar agora</span>
+                            <span class="icon"><i class="fa fa-arrow-right" aria-hidden="true"></i></span>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+    
+        </section>
+    
+        <section id="albums-section">
+    
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <h2>Receba um álbum personalizado todo mês com seus momentos mais importantes.</h2>
+                        <p>Garantimos álbuns personalizados todo mês de acordo com as estações do ano, eventos e festividades.</p>
+                    </div>
+                </div>
+            </div>
+    
+        </section>
 
-    </section>
+    </article>
+
 
 @endsection
