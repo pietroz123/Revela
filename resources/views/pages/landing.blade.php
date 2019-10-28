453a24a224d167aa2e7c0002b836f2d9b3e67ae1
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-10-28 16:55:49 -0300

    Inicio do design responsivo
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -13,7 +13,7 @@
     
             <div class="container">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-12 col-md-9 col-lg-6">
                         <h2>Tenha todas as suas fotos na palma da sua mão. Literalmente.</h2>
                         <p>Revela é a plataforma que te ajuda a liberar espaço no seu celular, colocando-as em um álbum digital todo mês.</p>
                         <a class="cta-home mt-3" href="#!">
@@ -30,7 +30,7 @@
     
             <div class="container">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-12 col-md-9 col-lg-6">
                         <h2>Receba um álbum personalizado todo mês com seus momentos mais importantes.</h2>
                         <p>Garantimos álbuns personalizados todo mês de acordo com as estações do ano, eventos e festividades.</p>
                     </div>
