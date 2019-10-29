d79bac26bb997fd9932357072bf97d18e30b030c
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-10-29 14:24:36 -0300

    Design responsivo na imagem de fundo (ajustar a imagem de fundo da hero-section

--- resources/js/pages/landing.js
+++ resources/js/pages/landing.js
@@ -7,21 +7,25 @@ $(document).ready(function() {
         $('html, body').animate({
             scrollTop: 0
         }, 1500);
+        $('.navbar-collapse').removeClass('show');
     });
     $('.nav-link.main-section').click(function() {
         $('html, body').animate({
             scrollTop: $("#albums-section").offset().top - 100
         }, 1500);
+        $('.navbar-collapse').removeClass('show');
     });
     $('.nav-link.steps-section').click(function() {
         $('html, body').animate({
             scrollTop: $("#steps-section").offset().top - 50
         }, 1500);
+        $('.navbar-collapse').removeClass('show');
     });
     $('.nav-link.plans-section').click(function() {
         $('html, body').animate({
             scrollTop: $("#plans-section").offset().top - 50
         }, 1500);
+        $('.navbar-collapse').removeClass('show');
     });
 
     /**
